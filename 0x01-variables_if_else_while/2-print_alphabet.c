9266466b2189575116cc8b4b2a1ecf3e767433f4
emmanuel <emmanuelnwafor212@gmail.com>
2022-04-20 06:47:27 -0700

    fix task 04

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,5 +13,5 @@ for (i = 0; i < 26; i++)
 putchar(alp[i]);
 }
 putchar('\n');
-return (0); 
+return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,6 +12,7 @@ int i;
 for (i = 97; i < 123; i++)
 {
 if (i != 101 && i != 113)
+{
 putchar(i);
 }
 }
