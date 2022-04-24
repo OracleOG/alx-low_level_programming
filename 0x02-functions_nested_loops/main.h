3f22a1025907ea75d4de0e423a926c7660d972d2
emmanuel <emmanuelnwafor212@gmail.com>
2022-04-24 05:31:35 -0700

    all projects on 0x02 nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,17 @@
 #ifndef _main_h_
 #define _main_h_
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
+void print_alphabet_x10(void);
 void _puts(char *str);
+void print_alphabet(void);
 int _putchar(char);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 #endif
