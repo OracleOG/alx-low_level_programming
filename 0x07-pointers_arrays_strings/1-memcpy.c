8dcefa12761b0dd8b00fdb1fcdf69a5e24f47a77
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2022-07-20 05:18:09 +0100

    first input for 0x07

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,22 @@
 #include "main.h"
+
 /**
-* _memcpy - copies memory area,
-* @dest: destination memory area.
-* @src: source memory area.
-* @n: bytes filled.
-* Return: the pointer to dest.
-*/
+ * _memcpy - copies memory area 
+ * @dest - destination string
+ * @src - source string
+ * @n - size of src to be copied
+ *
+ * return - dest string
+ **/
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
-*(dest + i) =  *(src + i);
-return (dest);
+	unsigned int count;
+
+	for(count = 0; count < n; count++)
+	{
+		*(dest + count) = *(src + count);
+	}
+
+	return (dest);
 }
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,28 @@
 #include "main.h"
+
 /**
-* _strchr - locates a character in a string,
-* @s: string.
-* @c: character.
-* Return: the pointer to the first occurrence of the character c.
-*/
+ * _strchr - searches for a character in a string
+ * @s - input string
+ * @C - character to be located
+ *
+ * return - pointer to the first occurrence of c if true
+ * if false returns NULL
+ **/
+
 char *_strchr(char *s, char c)
 {
-unsigned int i = 0;
-for (; *(s + i) != '\0'; i++)
-if (*(s + i) == c)
-return (s + i);
-if (*(s + i) == c)
-return (s + i);
-return ('\0');
+	int count, x = 0;
+
+	for(count = 0; s[count] != '\0'; count++)
+	{
+		if(s[count] == c)
+		{
+			x = count;
+			break;
+		}
+	}
+	if(x > 0)
+		return (*(s + x));
+	else
+		return ('NULL');
 }
