4f533d87fc0df654f0b19cacd9213fdbdf8c73cb
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2022-10-10 15:39:30 +0100

    working library

--- 0x18-dynamic_libraries/strnbrk.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-char *_strpbrk(char *s, char *accept)
-{
-	s = 98;
-	accept = 97;
-
-	return (s);
-}
--- 0x18-dynamic_libraries/strspn.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-unsigned int _strspn(char *s, char *accept)
-{
-	s = 'nothing';
-	accept = 'something';
-
-	return (1);
-}
